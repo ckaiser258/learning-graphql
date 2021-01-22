2c5a284a1c9c436559cff29c2956db796fd75baa
Colton Kaiser <kaiserc2@seattleu.edu>
2021-01-22 10:54:07 -0800

    Add express-graphql and begin to create request schema.

--- server/app.js
+++ server/app.js
@@ -1,6 +1,14 @@
 const express = require('express')
+const { graphqlHTTP } = require("express-graphql")
 
 const app = express()
+
+//Fire graphqlHTTP any time a request to '/graphql' comes in
+app.use('/graphql', graphqlHTTP({
+    //Define schema
+    
+
+}))
 
 app.listen(4000, () => {
     console.log('now listening for requests on port 4000')
