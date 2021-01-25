d386819e76a7663dbd8a54a8fdf05379a61c318d
Colton Kaiser <kaiserc2@seattleu.edu>
2021-01-25 09:47:47 -0800

    Create root query and format.

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,12 +1,37 @@
-const graphql = require("graphql")
+const graphql = require("graphql");
 
-const { GraphQLObjectType, GraphQLString } = graphql
+const { GraphQLObjectType, GraphQLString, GraphQLSchema } = graphql;
 
 const BookType = new GraphQLObjectType({
-    name: 'Book',
-    fields: () => ({
-        id: { type: GraphQLString },
-        name: { type: GraphQLString },
-        genre: { type: GraphQLString }
-    })
-})
+  name: "Book",
+  fields: () => ({
+    id: { type: GraphQLString },
+    name: { type: GraphQLString },
+    genre: { type: GraphQLString },
+  }),
+});
+
+//Define the root query which is how you will initially jump into the graph
+const RootQuery = new GraphQLObjectType({
+  name: "RootQueryType",
+  fields: {
+    book: {
+      type: BookType,
+      //args are what the user will need to pass in when making the call
+      //in this case they will need to specify an id in the query
+      //ex: book(id: '123'){
+      //      name
+      //      genre
+      //    }
+      args: { id: { type: GraphQLString } },
+      resolve(parent, args) {
+        //code to get data from db/other source
+      },
+    },
+  },
+});
+
+//Export the query of this schema to use in graphqlHTTP() in app.js
+module.exports = new GraphQLSchema({
+  query: RootQuery,
+});
