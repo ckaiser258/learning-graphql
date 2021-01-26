1b2b8b70d8a286bda2987063bf25f8041745ecf6
Colton Kaiser <kaiserc2@seattleu.edu>
2021-01-26 11:12:15 -0800

    Add dummy data and begin resolve() function.

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -2,6 +2,20 @@ const graphql = require("graphql");
 
 const { GraphQLObjectType, GraphQLString, GraphQLSchema } = graphql;
 
+//dummy data
+var books = [
+  {
+    name: "Name of the Wind",
+    genre: "Fantasy",
+    id: "1",
+  },
+  {
+    name: "The Final Empire",
+    genre: "Sci-Fi",
+    id: "2",
+  },
+];
+
 const BookType = new GraphQLObjectType({
   name: "Book",
   fields: () => ({
@@ -26,6 +40,7 @@ const RootQuery = new GraphQLObjectType({
       args: { id: { type: GraphQLString } },
       resolve(parent, args) {
         //code to get data from db/other source
+        return books.find((book) => args.id === book.id);
       },
     },
   },
