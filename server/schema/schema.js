ef13fe30f527715ac1aa38ce3dffea85d2983f05
Colton Kaiser <kaiserc2@seattleu.edu>
2021-01-27 10:53:00 -0800

    Change string type to id type for id parameter.

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,6 +1,6 @@
 const graphql = require("graphql");
 
-const { GraphQLObjectType, GraphQLString, GraphQLSchema } = graphql;
+const { GraphQLObjectType, GraphQLString, GraphQLSchema, GraphQLID } = graphql;
 
 //dummy data
 var books = [
@@ -19,7 +19,9 @@ var books = [
 const BookType = new GraphQLObjectType({
   name: "Book",
   fields: () => ({
-    id: { type: GraphQLString },
+    //The GraphQLID type will coerce a number to a string.
+    //It allows for you to pass a string OR a number as the id.
+    id: { type: GraphQLID },
     name: { type: GraphQLString },
     genre: { type: GraphQLString },
   }),
@@ -37,7 +39,7 @@ const RootQuery = new GraphQLObjectType({
       //      name
       //      genre
       //    }
-      args: { id: { type: GraphQLString } },
+      args: { id: { type: GraphQLID } },
       resolve(parent, args) {
         //code to get data from db/other source
         return books.find((book) => args.id === book.id);
