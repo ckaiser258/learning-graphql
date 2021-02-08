885ca8e16431344568df33daa9db38abd4c71f4a
Colton Kaiser <kaiserc2@seattleu.edu>
2021-02-08 10:09:36 -0700

    Add mongoose package and MongoDB functionality.

--- server/app.js
+++ server/app.js
@@ -1,8 +1,18 @@
 const express = require("express");
 const { graphqlHTTP } = require("express-graphql");
 const schema = require("./schema/schema");
+const mongoose = require("mongoose");
+const MongoClient = require("mongodb").MongoClient;
 
 const app = express();
+
+const uri =
+  "mongodb://user:12345@cluster0-shard-00-00.uwgnz.mongodb.net:27017,cluster0-shard-00-01.uwgnz.mongodb.net:27017,cluster0-shard-00-02.uwgnz.mongodb.net:27017/Cluster0?ssl=true&replicaSet=atlas-g8uqlj-shard-0&authSource=admin&retryWrites=true&w=majority";
+
+mongoose.connect(uri);
+mongoose.connection.once("open", () => {
+  console.log("connected to database");
+});
 
 //Fire graphqlHTTP any time a request to '/graphql' comes in
 app.use(
