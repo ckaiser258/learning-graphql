330fa26b8154c720878e9ae22b4c7824f0dcbfa6
Colton Kaiser <kaiserc2@seattleu.edu>
2021-02-15 13:52:02 -0700

    Create BookList component.

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,10 @@
+import BookList from "./components/BookList";
+
 function App() {
   return (
     <div className="main">
       <h1>Colton's Reading List</h1>
+      <BookList />
     </div>
   );
 }
