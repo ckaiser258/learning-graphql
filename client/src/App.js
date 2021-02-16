17f32146fd0c31ff20d75cd28665acf8536ec1dc
Colton Kaiser <kaiserc2@seattleu.edu>
2021-02-16 12:41:02 -0700

    Set up ApolloClient and ApolloProvider.

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,23 @@
 import BookList from "./components/BookList";
+import ApolloClient from "apollo-boost";
+import { ApolloProvider } from "react-apollo";
+
+//apollo client setup
+const client = new ApolloClient({
+  //uri is where we will make queries to
+  uri: "http://localhost:4000/graphql",
+});
 
 function App() {
   return (
-    <div className="main">
-      <h1>Colton's Reading List</h1>
-      <BookList />
-    </div>
+    //wrap app in ApolloProvider and set the client
+    //this allows us to take the data coming from the endpoint and inject it into the app
+    <ApolloProvider client={client}>
+      <div className="main">
+        <h1>Colton's Reading List</h1>
+        <BookList />
+      </div>
+    </ApolloProvider>
   );
 }
 
