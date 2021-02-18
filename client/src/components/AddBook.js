8a9a0603828fd18342d010f99ae91085899d89f6
Colton Kaiser <kaiserc2@seattleu.edu>
2021-02-18 11:29:27 -0700

    Create AddBook component and form.

---
+++ client/src/components/AddBook.js
@@ -0,0 +1,34 @@
+import { gql } from "apollo-boost";
+import { graphql } from "react-apollo";
+
+const getAuthorsQuery = gql`
+  {
+    authors {
+      name
+      id
+    }
+  }
+`;
+
+function AddBook(props) {
+  return (
+    <form id="add-book">
+      <div className="field">
+        <label>Book name:</label>
+        <input type="text" />
+      </div>
+
+      <div className="field">
+        <label>Genre:</label>
+        <input type="text" />
+      </div>
+
+      <div className="field">
+        <select>
+          <option>Select author</option>
+        </select>
+      </div>
+      <button>+</button>
+    </form>
+  );
+}
